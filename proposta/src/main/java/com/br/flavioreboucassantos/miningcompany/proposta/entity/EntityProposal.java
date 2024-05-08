208ed47ec9f5260235b2ac0733af619c525d7fa0
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2024-05-08 14:21:30 -0300

    Under Construction
    Upgrades JDK version to 21.
    Fix file.

--- proposta/src/main/java/com/br/flavioreboucassantos/miningcompany/proposta/entity/EntityProposal.java
+++ proposta/src/main/java/com/br/flavioreboucassantos/miningcompany/proposta/entity/EntityProposal.java
@@ -47,12 +47,11 @@ public class EntityProposal extends PanacheEntityBase {
 	}
 
 	public EntityProposal(final DtoProposalDetails dtoProposalDetails) {
-//		customer = dtoProposalDetails.customer();
-//		priceTonne = dtoProposalDetails.priceTonne();
-//		tonnes = dtoProposalDetails.tonnes();
-//		country = dtoProposalDetails.country();
-//		proposalValidityDays = dtoProposalDetails.proposalValidityDays();
-		dtoProposalDetails(customer, priceTonne, tonnes, country, proposalValidityDays);
+		customer = dtoProposalDetails.customer();
+		priceTonne = dtoProposalDetails.priceTonne();
+		tonnes = dtoProposalDetails.tonnes();
+		country = dtoProposalDetails.country();
+		proposalValidityDays = dtoProposalDetails.proposalValidityDays();
 		created = new Date();
 	}
 
